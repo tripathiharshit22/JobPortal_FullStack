51a2fd6193c4d6f1c63ae51d7437f90b5f5232f9
HarshitManiTripathi <harshit2213114@akgec.ac.in>
2025-09-21 00:37:21 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-fullstack-6338.onrender.com',
     credentials:true
 }
 
